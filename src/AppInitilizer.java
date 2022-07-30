9ff0ba1487f05396fd1c75db201bfd1813813831
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-30 13:08:20 +0530

    Add Client Managemant UI and Controller

---
+++ src/AppInitilizer.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitilizer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
