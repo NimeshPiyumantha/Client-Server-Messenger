82b923577d1eafe81073ab9bcae1a5dcce9964ab
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-11 11:52:22 +0530

    Refactor and add Code

--- src/ClientAppInitilaizer.java
+++ src/ClientAppInitilaizer.java
@@ -5,10 +5,14 @@
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ClientAppInitilaizer extends Application {
 
@@ -19,9 +23,27 @@ public class ClientAppInitilaizer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        primaryStage.setScene(new Scene(FXMLLoader.load((getClass().getResource("com/Nimesh/sokectProgramming/views/ClientManagemant.fxml")))));
-        primaryStage.setTitle("Client");
-      //  primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("com/Nimesh/sokectProgramming/views/ClientManagemant.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        anableMove(scene, primaryStage);
         primaryStage.show();
+    }
+
+    private void anableMove(Scene scene, Stage primaryStage) {
+        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+        scene.setOnMousePressed(event -> {
+            xOffset.set(primaryStage.getX() - event.getScreenX());
+            yOffset.set(primaryStage.getY() - event.getScreenY());
+        });
+        //Lambda mouse event handler
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset.get());
+            primaryStage.setY(event.getScreenY() + yOffset.get());
+        });
     }
 }
--- src/SeverAppInitilaizer.java
+++ src/SeverAppInitilaizer.java
@@ -5,11 +5,14 @@
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SeverAppInitilaizer extends Application {
 
@@ -20,10 +23,27 @@ public class SeverAppInitilaizer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        primaryStage.setScene(new Scene(FXMLLoader.load((getClass().getResource("com/Nimesh/sokectProgramming/views/SeverManagemant.fxml")))));
-        primaryStage.setTitle("Sever");
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("com/Nimesh/sokectProgramming/views/SeverManagemant.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        anableMove(scene, primaryStage);
         primaryStage.show();
+    }
 
+    private void anableMove(Scene scene, Stage primaryStage) {
+        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+        scene.setOnMousePressed(event -> {
+            xOffset.set(primaryStage.getX() - event.getScreenX());
+            yOffset.set(primaryStage.getY() - event.getScreenY());
+        });
+        //Lambda mouse event handler
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset.get());
+            primaryStage.setY(event.getScreenY() + yOffset.get());
+        });
     }
 }
